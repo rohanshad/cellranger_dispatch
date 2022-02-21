e6a33a7420bdb08525e745051e541d37598aece5
Rohan Shad <rohan.shad@gmail.com>
2022-02-20 16:52:27 -0800

    Initial commit: Add readme, requirements, and cellranger_dispatch.py

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# cellranger_dispatch
-Fire and forget python wrapper for single cell RNA pre-processing with cellranger
+# Cellranger Dispatch
+A somewhat fully contained "Fire and forget" python wrapper for single cell RNA pre-processing with cellranger.
+
+***
+
+#### Installation and Setup
+
+Tested on Ubuntu 20.02 and CentOS7 
+These steps need to be done once. 
+
+1. If working on Sherlock the conda environments are already setup
+
+	```
+	source activate rnaseq
+	```
+2. Install dependencies with pip (skip if on hiesinger sherlock partition)
+
+	```
+	pip install -r requirements.txt
+	```
+3. Ensure cellranger works and you can access it from the terminal. Add these two lines to your bashrc file
+
+	```
+	vim ~/.bashrc
+	```
+	Hit 'i' on the keyboard and add these two lines somewhere:
+
+	```
+	export PATH="$GROUP_HOME/single_cell/cellranger-6.1.2:$PATH"
+	export PATH="$GROUP_HOME/single_cell/cellranger-atac-1.2.0:$PATH"
+	```
+
+***
+
+#### Running cellranger_dispatch
+
+The program copies over data from the genome sequencing service center (Usually a folder within `"/oak/stanford/projects/genomics/labs/mfischbe"`) into our own `$OAK` or `$GROUP_SCRATCH` directory of choice. There after passing some checks and reading in the provided samples.csv file, the script will build FASTQ files using all available CPU cores. A separate batch submit script is generated for each sample, and dispatched to the SLURM job scheduler to run either atac or RNAseq alignment. All count matrices and job outputs are delivered to a folder on `$GROUP_SCRATCH` to a folder called `RNA_seq_outputs_{DD_MM_YY}`. 
+
+
+The program takes requires the following arguments to run (```python cellranger_dispatch.py --help``` to see more)
+
+```
+  -i , --input_source   Name of raw data dump folder on /oak/stanford/projects/genomics/labs/mfischbe
+  -r , --root_dir       Full path to new data directory on $OAK or $GROUP_SCRATCH
+  -s , --sample_csv     Name of samples.csv file in the correct format
+  -g , --genome         Name of the genome eg: refdata-cellranger-mm10-3.0.0
+  -t , --task           set as either "rna" or "atac"
+  -d , --debug 			
+```
+
+#### Example usage:
+
+1. The script should be run within a sherlock compute node with adequate CPU and RAM for a time duration that covers the generation of FASTQ files. If you try running ```cellranger_dispatch.py``` within a login node it will error out.
+
+	```
+	salloc -p willhies -c 12 --mem 96GB -t 12:00:00 -J rohans_legacy
+	```
+
+2. Load the conda environment in the compute node
+
+	```
+	source activate rnaseq
+	```
+
+3. Run the program with appropriate inputs
+
+	```
+	python cellranger_dispatch.py -i 220127_A00509_0436_AH5CHKDMXY -r '/scratch/groups/willhies/testing/2_19_2022' -s mouse_samples.csv -t rna -g refdata-cellranger-mm10-3.0.0
+	```
+
+The program can resume from where it was interrupted, whether while copying data over to a new directory, or while building FASTQ files. Re-run the command and let it do it's work. This should work 95% of the time straight out of the box with minimal tinkering. If it doesn't then debugging will be fun. 
+
+
+#### Debugging:
+
+1. Where is the data? 
+	Check the way you've named the folders is wrong or it points to the wrong location. The program prints out what it has parsed from your inputs, if those folder names look wrong then you know what to fix.
+
+2. csv formatting issues
+	If your csv file is incorrectly formatted or not present in the same location as where you ran the `cellranger_dispatch.py` script from, it won't work and will break. The script will thankfully tell you if it has any problems with the csv file. 
+
+3. Rsync inode / IOerrors / AllocationErrors:
+	Seen sometimes when moving data within `$OAK`, if this happens contant sherlock support and ask for help and forgiveness. 
+
+4. Runs too slow? 
+	Each sample for cellranger count / cellranger-atac count is setup to run across 12 cores and 72GB RAM. You can bump this up manually by changing the hardcoded defaults. 
+
+
---
+++ cellranger_dispatch.py
@@ -0,0 +1,304 @@
+'''
+Master script to generate folder structure, build fastqs, and run cellranger pipeline
+'''
+
+import argparse as ap
+import subprocess
+from shutil import move, copyfile, copytree
+import os 
+import pandas as pd
+import tarfile
+from datetime import date
+import time
+import glob
+import bcolors
+import sys
+import textwrap
+
+class RNA_Pipeline_Run:
+	def __init__(self, input_dir, root, sample_csv_file, task, genome, debug):
+
+		self.input_dir = os.path.join('/oak/stanford/projects/genomics/labs/mfischbe', input_dir)
+		#self.root_dir = os.path.join('/oak/stanford/groups/willhies/rna_seq_rawdata', root)
+		self.root_dir = root
+		self.sample_csv_file = os.path.join(os.getcwd(),sample_csv_file)
+		self.task = task
+		self.genome = os.path.join('$OAK/ref_genomes', genome)
+
+		# Create these folders at later stage
+		self.startup_generator = os.path.join(self.root_dir, 'startup_generator')
+		self.mkfastq_dir = os.path.join('mkfastq_dir')
+
+	def run_cellranger_atac_count(self, sample, genome):
+		'''
+		Builds command for cellranger-atact count + output directory delivery
+		'''
+		formatting_bs = textwrap.dedent(f'''\
+		#Creates working directory within $GROUP_SCRATCH + runs everything in it
+		mkdir $GROUP_SCRATCH/workdir
+		cd $GROUP_SCRATCH/workdir
+
+		echo "Running in GROUP_SCRATCH"	
+		''')
+		
+		cellranger_cmd = 'cellranger-atac count --id={sample}' \
+				' --transcriptome={genome}' \
+				' --fastqs={fastqs}' \
+				' --sample={sample}' \
+				' --expect-cells=10000'.format(
+					sample = sample,
+					genome = self.genome,
+					fastqs = os.path.join(self.root_dir,self.mkfastq_dir,'outs','fastq_path')
+					)
+		copy_outputs = textwrap.dedent(f'''\
+			
+			echo "Moving to GROUP_SCRATCH"
+			mkdir $GROUP_SCRATCH/RNA_seq_outputs_{date.today().strftime("%d_%m_%y")}/
+			mv $GROUP_SCRATCH/workdir/{sample} $GROUP_SCRATCH/RNA_seq_outputs_{date.today().strftime("%d_%m_%y")}/
+
+			echo "Job copied, waiting 10s to terminate..."
+
+			sleep 10
+			''')
+			
+		run_command = formatting_bs + cellranger_cmd + copy_outputs	
+		return cellranger_cmd
+
+	def run_cellranger_count(self, sample, genome):
+		'''
+		Builds command for cellranger count + output directory delivery
+		'''
+		formatting_bs = textwrap.dedent(f'''\
+		#Creates working directory within high speed L_SCRATCH + runs everything in it
+		mkdir $L_SCRATCH/workdir
+		cd $L_SCRATCH/workdir
+
+		echo "Running in L_SCRATCH"	
+		''')
+		
+		cellranger_cmd = 'cellranger count --id={sample}' \
+				' --transcriptome={genome}' \
+				' --fastqs={fastqs}' \
+				' --sample={sample}' \
+				' --expect-cells=10000'.format(
+					sample = sample,
+					genome = self.genome,
+					fastqs = os.path.join(self.root_dir, self.mkfastq_dir,'outs','fastq_path')
+					)
+		copy_outputs = textwrap.dedent(f'''\
+			
+			
+			#L_SCRATCH wil purge everything from memory as soon as job ends, this step copies everything back to GROUP_SCRATCH
+
+			echo "Copying back to GROUP_SCRATCH"
+
+			mkdir $GROUP_SCRATCH/RNA_seq_outputs_{date.today().strftime("%d_%m_%y")}/
+			cp -r $L_SCRATCH/workdir/{sample} $GROUP_SCRATCH/RNA_seq_outputs_{date.today().strftime("%d_%m_%y")}/
+
+			echo "Job copied, waiting 10s to terminate..."
+
+			sleep 10
+			''')
+		run_command = formatting_bs + cellranger_cmd + copy_outputs	
+		return run_command
+
+	def build_slurm_scripts(self, samples_csv_file, task):
+		'''
+		Builds slurm scripts for all samples in a for loop
+		'''
+
+		sample_df = pd.read_csv(samples_csv_file)
+		for sample in sample_df['Sample'].unique():
+			slurm_header = textwrap.dedent('''\
+			#!/bin/bash
+			#SBATCH --job-name={sample}
+			#SBATCH --output={sample}.log
+			#SBATCH --mail-user=rshad@stanford.edu
+			#SBATCH --mail-type=end
+
+			# Time limits 
+			#SBATCH -t 24:00:00
+
+			# Partition info
+			#SBATCH --partition=owners,willhies
+			#SBATCH --qos=normal
+			#SBATCH --nodes=1
+			#SBATCH --mem=72GB
+			#SBATCH --ntasks-per-node=12
+
+			ulimit -u 10000
+			'''.format(sample=sample))
+
+			if task == 'rna':
+				run_command = self.run_cellranger_count(sample, self.genome)
+
+			elif task == 'atac':
+				run_command = self.run_cellranger_atac_count(sample+'_atac', self.genome)
+
+
+			file_path = os.path.join(self.root_dir, 'startup_generator', sample, 'submit.sh')
+			with open(file_path, 'w') as file:
+				# Creates new file to prevent double overwrites
+				file.write(slurm_header)
+			with open(file_path, 'a') as file:
+				# Appends run command to newly created file
+				file.write(run_command)
+
+		print(f'{bcolors.OK}Successfully generated runfiles!{bcolors.ENDC}')
+		print(f'Run files generated for {len(sample_df["Sample"].unique())} samples')
+		
+
+	def check_inputs(self, samples_csv_file):
+		print('Checking csv sample inputs for formatting errors...')
+		sample_df = pd.read_csv(self.sample_csv_file)
+
+
+	def master_run(self):
+		submit_files = glob.glob(os.path.join(self.root_dir, 'startup_generator','*','*.sh'))
+		for i in submit_files:
+			print(f'Job for {(i.split("startup_generator/"))[1].split("/submit.sh")[0]}:')
+			subprocess.run('sbatch {file}'.format(file=i), shell=True)
+
+
+	def untar_files(self):
+		for f in os.listdir(self.root_dir):
+			if f[-3:] == 'tar':
+				tar = tarfile.open(os.path.join(self.root_dir, filename))
+				tar_extract_path = os.path.join(self.root_dir, filename[:-4])
+				tar.extractall(tar_extract_path)
+				tar.close()
+				print(f'Extracted {f}')
+				move(os.path.join(self.root_dir, f), self.archive)
+
+			elif f[-6:] == 'tar.gz':
+				tar = tarfile.open(os.path.join(self.root_dir, filename))
+				tar_extract_path = os.path.join(self.root_dir, filename[:-7])
+				tar.extractall(tar_extract_path)
+				tar.close()
+				print(f'Extracted {f}')
+				move(os.path.join(self.root_dir, f), self.archive)
+				
+			else:
+				pass
+
+
+	def prep_root_directory(self):
+		start_time = time.time()
+		
+		print(f'Copying data from genomics core directory {self.input_dir}')
+		print(f'{bcolors.BLUE}This will take a while, grab a redbull or something...{bcolors.ENDC}')
+
+		os.makedirs(self.root_dir, exist_ok=True)
+		subprocess.run('rsync -azhv {src}/ {dest} --info=progress2 --info=name0'.format(src=self.input_dir, dest=self.root_dir), shell=True)
+
+		print(f'Copying complete, elapsed time: {round((time.time() - start_time), 2)}s')
+		print('Preparing working directory structure')
+
+		os.makedirs(self.startup_generator, exist_ok=True)
+
+	def make_fastqs(self, samples_csv_file):
+		'''
+		Generates FASTQ files based on the data copied over from the sequencing core and the csv file.
+		'''
+		sample_df = pd.read_csv(self.sample_csv_file)
+		for sample in sample_df['Sample']:
+			os.makedirs(os.path.join(self.root_dir, 'startup_generator', sample), exist_ok=True)
+
+		print('Directory structure created')
+
+		os.chdir(self.root_dir)
+		mkfastq_command = 'cellranger mkfastq --id="{mkfastq_dir}"' \
+					' --run="{main_folder}"' \
+					' --csv="{sample_csv_file}"'\
+					' --jobmode="local"' \
+					' --rc-i2-override=True'.format(
+						mkfastq_dir=self.mkfastq_dir,
+						main_folder=self.root_dir,
+						sample_csv_file=self.sample_csv_file
+					)
+		
+		print(mkfastq_command)
+
+		bcl2fastq_load = 'module load biology bcl2fastq'
+		p = subprocess.Popen('{cmd1};{cmd2}'.format(cmd1=bcl2fastq_load, cmd2=mkfastq_command), shell=True)
+		streamdata = p.communicate()[1]
+		
+		if p.returncode != 0:
+			print(f'{bcolors.ERR}Fastq generation FAILED{bcolors.ENDC}')
+			sys.exit()
+		else:
+			print(f'{bcolors.OK}Fastqs created!{bcolors.ENDC}')
+
+
+	def run(self):
+		'''
+		Initializes pipeline sequentially
+		'''
+		print('------------------------------------')
+		print(f'PREPARING ROOT DIRECTORY ON OAK')
+		print('------------------------------------')
+		self.prep_root_directory()
+		self.untar_files()
+		print('------------------------------------')
+		print(f'BUILDING FASTQ FILES')
+		print('------------------------------------')
+		self.make_fastqs(self.sample_csv_file)
+		print('------------------------------------')
+		print(f'BUILDING RUNFILES')
+		print('------------------------------------')
+		self.build_slurm_scripts(self.sample_csv_file, self.task)
+		print('------------------------------------')
+		print(f'DISPATCHING CELLRANGER JOBS')
+		print('------------------------------------')
+		self.master_run()
+
+
+
+if __name__ == '__main__':
+	parser = ap.ArgumentParser(
+		description="Automates a bunch of stuff I used to do for Fischbein Lab. \
+			Sequentially will generate folder structure for a successful cellranger run in the given {--root_dir}. \
+			Raw reads are first copied over from {--input_dir} following which cellranger mkfastq and cellranger count \
+			(either RNAseq or ATACseq) will be distributed across muiltiple nodes. The sampel ids are pulled from a user defined \
+			csv file {--sample_csv}. The outputs will appear in $GROUP_SCRATCH/RNA_seq_outputs with a datestamp",
+
+		epilog="Version 1.0; Created by Rohan Shad, MD"
+	)
+
+	parser.add_argument('-i', '--input_source', metavar='', required=True, help='Name of raw data dump folder on /oak/stanford/projects/genomics/labs/mfischbe')
+	parser.add_argument('-r', '--root_dir', metavar='', required=True, help='Full path to root data directory on OAK')
+	parser.add_argument('-s', '--sample_csv', metavar='', required=True, help='Name of samples.csv file in the correct format (save this in root_dir)')
+	parser.add_argument('-g', '--genome', metavar='', required=True, help='Name of the genome eg: refdata-cellranger-mm10-3.0.0')
+	parser.add_argument('-t', '--task', metavar='', required=True, help='set as either "rna" or "atac"')
+	parser.add_argument('-d', '--debug', metavar='', required=False, default=False)
+
+	args = vars(parser.parse_args())
+	print(args)
+
+	
+	input_dir = args['input_source']
+	root_dir = args['root_dir']
+	sample_csv = args['sample_csv']
+	genome = args['genome']
+	task = args['task']
+	debug = args['debug']
+
+	print(f'Using genome: {bcolors.BLUE}{genome}{bcolors.END}')
+	print(f'Processing experiment: {bcolors.BLUE}{task}{bcolors.END}')
+
+	rna_pipe = RNA_Pipeline_Run(input_dir, root_dir, sample_csv, task, genome, debug)
+
+	start_time = time.time()
+	
+	### START RUN JOB ### 
+	rna_pipe.run()
+
+
+	print(f'{bcolors.OK}Jobs submitted on slurm{bcolors.END}. Type in "squeue -u your_sunet_here" to check for status')
+	print(f'Output files will be delivered to $GROUP_SCRATCH/RNA_seq_outputs_{date.today().strftime("%d_%m_%y")}')
+	print(f'Elapsed time: {round((time.time() - start_time), 2)}s')
+
+
+
+
+
